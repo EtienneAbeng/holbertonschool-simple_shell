a42feaa7bfe3e41cbc14ef5e696340de33249341
EtienneAbengclear <abengetienne@gmail.com>
2023-12-20 07:12:09 -0800

    buffer

--- buffer.c
+++ buffer.c
@@ -1,4 +1,9 @@
 #include "shell.h"
+/**
+ * buffer - function that print the buffer
+ * at the begining of the line
+ * Return - nothing
+ */
 void buffer() 
 {const char *prompt = "shell >>";
 	size_t prompt_length = strlen(prompt);
